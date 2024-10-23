24519890292b793bf13f531ab2797c408497698f
alena0604 <gericimo87@gmail.com>
2024-10-23 22:18:52 +0200

    added difference k

---
+++ array_tasks/difference_k.py
@@ -0,0 +1,17 @@
+# Given an integer array nums and an integer k,
+# return the number of pairs (i, j) where i < j such
+# that |nums[i] - nums[j]| == k.
+from typing import List
+
+
+def count_k_difference(nums: List[int], k: int) -> int:
+    result = 0
+
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if abs(nums[i] - nums[j]) == k:
+                result += 1
+    return result
+
+
+print(count_k_difference([1,2,2,1], 1))
