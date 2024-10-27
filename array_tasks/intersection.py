7a04921648eec5d8c30d7678cb279faba73721d6
alena0604 <gericimo87@gmail.com>
2024-10-27 21:08:40 +0100

    intersection

--- array_tasks/intersection.py
+++ array_tasks/intersection.py
@@ -1,4 +1,9 @@
-# Given two arrays, write a function to get the intersection of the two.
+# 1. Given two arrays, write a function to get the intersection of the two.
+# 2. Given two integer arrays nums1 and nums2, return an array of their intersection.
+# Each element in the result must appear as many times as it shows in both arrays and
+# you may return the result in any order.
+from typing import List
+from collections import Counter
 
 
 def intersection(arr_one, arr_two):
@@ -6,6 +11,17 @@ def intersection(arr_one, arr_two):
 
 
 print(intersection([1, 2, 3, 4, 5], [0, 1, 3, 7]))
+
+
+def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
+    counter = Counter(nums1)
+    intersection = []
+
+    for i in nums2:
+        if counter[i] > 0:
+            intersection.append(i)
+            counter[i] -= 1
+    return intersection
 
 
 
