7df96a4e28a5595be6c4c636f9476211c7df9e88
alena0604 <gericimo87@gmail.com>
2025-09-01 21:39:43 +0200

    added reverse

--- linked_list/singly_linked_list.py
+++ linked_list/singly_linked_list.py
@@ -97,11 +97,63 @@ class LinkedList:
         return False
 
     def pop_first(self):
+        if self.length == 0:
+            return None
         popped_node = self.head
-        self.head = self.head.next
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            popped_node.next = None
+        self.length -= 1
+        return popped_node
+
+    def pop_last(self):
+        if self.length == 0:
+            return None
+        popped_node = self.tail
+        if self.length == 1:
+            self.head = self.tail = None
+        else:
+            temp = self.head
+            while temp.next is not self.tail:
+                temp = temp.next
+            self.tail = temp
+            temp.next = None
+        self.length -= 1
+        return popped_node
+
+    def remove(self, index):
+        if index >= self.length or index < -1:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1 or index < -1:
+            return self.pop_last()
+        prev_node = self.get(index - 1)
+        popped_node = prev_node.next
+        prev_node.next = popped_node.next
         popped_node.next = None
         self.length -= 1
         return popped_node
+
+    def delete_all(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def reverse(self):
+        prev = None
+        self.tail = self.head
+        current = self.head
+        while current:
+            nxt = current.next
+            current.next = prev  # flip pointer from next to prev
+            prev = current  # assign prev for the iteration
+            current = nxt   # 
+        self.head = prev
+
 
 
 if __name__ == "__main__":
