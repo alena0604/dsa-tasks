31907a0918f3e6dd460818c4696bba85c5da3b8d
alena0604 <gericimo87@gmail.com>
2025-09-07 11:49:41 +0200

    added stack implementation

--- stack/stack_python_list.py
+++ stack/stack_python_list.py
@@ -12,8 +12,18 @@ class Stack:
         values = [str(x) for x in reversed(self.items)]
         return '\n'.join(values)
 
+    def pop(self):
+        if self.is_empty():
+            return "Stack is empty"
+        return self.items.pop()
+
     def push(self, element):
         self.items.append(element)
+
+    def peak(self):
+        if self.is_empty():
+            return "Stack is empty"
+        return self.items[-1]
 
 
 
@@ -21,4 +31,6 @@ new_stack = Stack()
 new_stack.push(10)
 new_stack.push(20)
 new_stack.push(30)
-print(len(new_stack.items))
+print(new_stack)
+new_stack.pop()
+print(new_stack)
