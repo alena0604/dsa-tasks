a344697934a264808d2e7a013c5a078a404944f8
alena0604 <gericimo87@gmail.com>
2025-09-24 17:18:37 +0200

    added traversal

--- tree/binary_search_tree.py
+++ tree/binary_search_tree.py
@@ -6,6 +6,8 @@ class BSTNode:
 
 
 def insert_node(root_node, node_value):
+    # Time O(logN)
+    # Space O(logN)
     if root_node.data == None:
         root_node.data = node_value
     elif node_value <= root_node.data:
@@ -19,6 +21,36 @@ def insert_node(root_node, node_value):
         else:
             insert_node(root_node.right_child, node_value)
     return "success"
+
+
+def pre_order_traversal(root_node):
+    # Time O(n)
+    # Space O(n)
+    if not root_node:
+        return
+    print(root_node.data)
+    pre_order_traversal(root_node.left_child)
+    pre_order_traversal(root_node.right_child)
+
+
+def in_order_traversal(root_node):
+    # Time O(n)
+    # Space O(n)
+    if not root_node:
+        return
+    in_order_traversal(root_node.left_child)
+    print(root_node.data)
+    in_order_traversal(root_node.right_child)
+
+
+def post_order_traversal(root_node):
+    # Time O(n)
+    # Space O(n)
+    if not root_node:
+        return
+    post_order_traversal(root_node.left_child)
+    post_order_traversal(root_node.right_child)
+    print(root_node.data)
 
 
 new_bst = BSTNode(None)
