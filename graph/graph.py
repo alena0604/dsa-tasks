d75222c7af357962d0d99afa53287b9e8e04a915
alena0604 <gericimo87@gmail.com>
2025-10-05 20:50:38 +0200

    added graph creation

---
+++ graph/graph.py
@@ -0,0 +1,25 @@
+# A defaultdict automatically creates a default value for any key that doesn’t exist.
+
+# from collections import defaultdict
+
+# graph = defaultdict(list)
+
+
+class Graph:
+    def __init__(self, gdict=None):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+
+    def add_edge(self, vertex, edge):
+        if vertex in self.gdict:
+            self.gdict[vertex].append(edge)
+        else:
+            self.gdict[vertex] = [edge]
+
+
+new_dict = {'a': ['b', 'c'], 'b': ['d']}
+
+graph = Graph(new_dict)
+print(graph.gdict)
+    
