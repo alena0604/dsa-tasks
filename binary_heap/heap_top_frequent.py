1b269807ab9b450841850dd7cb9cb323e5db1026
alena0604 <gericimo87@gmail.com>
2025-11-29 16:18:04 +0100

    heap top n frequent

---
+++ binary_heap/heap_top_frequent.py
@@ -0,0 +1,20 @@
+# Top K Frequent Elements Count elements, build a heap of pairs (frequency, element).
+import heapq
+from collections import Counter
+
+
+def top_frequent_items(arr, n):
+
+    counter_freq = Counter(arr)
+    min_heap = []
+
+    for key, value in counter_freq.items():
+        heapq.heappush(min_heap, (value, key))
+
+        if len(min_heap) > n:
+            heapq.heappop(min_heap)
+
+    return [key for val, key in min_heap]
+
+
+print(top_frequent_items([1,5,5,2,2,3,5], 2))
