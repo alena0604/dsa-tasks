577dcbdd439ec7417b2b821c02887e743faed644
alena0604 <gericimo87@gmail.com>
2026-06-18 13:25:57 +0200

    added iterative

--- graphs/dfs_explained.py
+++ graphs/dfs_explained.py
@@ -12,5 +12,23 @@ def dfs(graph, node, visited=None):
             dfs(graph, neighbor, visited)   # Go deep into each neighbor
 
 
+def dfs_iterative(graph, node):
+    visited = set()
+    stack = [node]
+    result = []
+    while stack:
+        current_node = stack.pop()
+
+        if current_node not in visited:
+            visited.add(current_node)
+            result.append(current_node)
+
+        for neighbours in graph[current_node]:
+            if neighbours not in visited:
+                stack.append(neighbours)
+
+    return result
+
+
 graph = {0:[1,2], 1:[0,3,4], 2:[0,5,6], 3:[1], 4:[1], 5:[2], 6:[2]}
-print(dfs(graph, 0))
+print(dfs_iterative(graph, 0))
